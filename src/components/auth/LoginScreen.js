218b5ecc067dbc5a0322c9bd025be39da314b58d
S00651 <david.martin.peral@ibm.com>
2021-11-06 13:36:06 +0100

    Version inicial

---
+++ src/components/auth/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+export const LoginScreen = () => {
+
+    return(
+        <>
+            <h1>Login Screen</h1>
+            <hr/>
+        </>
+    )
+}
---
+++ src/components/journal/JournalScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const JournalScreen = () => {
+    return(
+        <>
+            <h1>Journal Screen</h1>
+            <hr/>
+        </>
+    )
+}
